92d9dcef7b9b0cb947a20ae58e3bcbdedf6e7384
CryoFlar3 <ndukes@computermentors.org>
2015-03-22 00:33:57 -0400

    Added RecycleView

--- app/src/main/java/computermentors/org/stormy/ui/DailyForecastActivity.java
+++ app/src/main/java/computermentors/org/stormy/ui/DailyForecastActivity.java
@@ -1,23 +1,34 @@
 package computermentors.org.stormy.ui;
 
 import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Parcelable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 
+import java.util.Arrays;
+
 import computermentors.org.stormy.R;
+import computermentors.org.stormy.adapters.DayAdapter;
+import computermentors.org.stormy.weather.Day;
 
 public class DailyForecastActivity extends ListActivity {
+
+    private Day[] mDays;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_forecast);
 
-        String[] daysOfTheWeek = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, daysOfTheWeek);
+        Intent intent = getIntent();
+        Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.DAILY_FORECAST);
+        mDays = Arrays.copyOf(parcelables, parcelables.length, Day[].class);
+
+        DayAdapter adapter = new DayAdapter(this, mDays);
         setListAdapter(adapter);
     }
 
